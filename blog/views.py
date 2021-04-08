9706d6266d483fda462b78d9a5a3e70e9393b2c5
Mithilesh-M <msmithilesh@yahoo.in>
2021-04-08 12:37:19 +0530

    Add index view for blog
    This gives home page as a response

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from .models import Blog, Blogger, Comment
 
-# Create your views here.
+
+
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_blogs = Blog.objects.all().count()
+    num_bloggers = Blogger.objects.all().count()
+
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 1)
+    request.session['num_visits'] = num_visits + 1
+
+    context = {
+        'num_blogs': num_blogs,
+        'num_bloggers': num_bloggers,
+        'num_visits': num_visits,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
